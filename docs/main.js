5395c5d2ec298f92fcf3af76f86819c755c80460
Low Front <lowfronterior@gmail.com>
2025-01-15 23:24:50 +0900

    chore: remove unsed file

--- docs/main.js
+++
@@ -1,7 +0,0 @@
-import { Terminal } from 'https://cdn.jsdelivr.net/npm/@xterm/xterm@5.5.0/+esm'
-import { WebglAddon } from 'https://cdn.jsdelivr.net/npm/@xterm/addon-webgl@0.18.0/+esm'
-
-var term = new Terminal()
-term.open(document.getElementById('terminal'))
-term.loadAddon(new WebglAddon())
-term.write('\x1B[1;3;31mCRH\x1B[0m $ ')
