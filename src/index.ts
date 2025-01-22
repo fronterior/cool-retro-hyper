47fdb36c66b6282726187e63cccd8068c384b605
Low Front <lowfronterior@gmail.com>
2025-01-22 22:43:14 +0900

    feat: add global css file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+if (globalThis.document) {
+  const link = Object.assign(document.createElement('link'), {
+    rel: 'stylesheet',
+    href: `${__dirname}/index.css`,
+  })
+
+  document.head.append(link)
+}
+
 export * from './decorateHyper'
 export * from './decorateMenu'
