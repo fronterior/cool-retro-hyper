39972ab5b679964e7233751efc2c8be54ac4ff61
Low Front <lowfronterior@gmail.com>
2025-02-03 03:36:36 +0900

    feat: add step line design

--- src/components/RotaryKnob/RotaryKnob.tsx
+++ src/components/RotaryKnob/RotaryKnob.tsx
@@ -1,8 +1,16 @@
-import React, { useCallback, useState } from 'react'
-import { Knob, UseKnobProps } from 'rotary-knob/react'
+import React, {
+  KeyboardEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+import { Knob } from 'rotary-knob/react'
 import styles from './RotaryKnob.module.css'
 
 interface RotaryKnobProps {
+  label: string
   defaultValue?: number
   value?: number
   minAngle?: number
@@ -16,6 +24,7 @@ interface RotaryKnobProps {
 }
 
 export function RotaryKnob({
+  label,
   defaultValue = 0,
   minAngle = 0,
   maxAngle = 270,
@@ -27,26 +36,113 @@ export function RotaryKnob({
   onValueChange,
 }: RotaryKnobProps) {
   const [value, setValue] = useState(defaultValue)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const validateValue = useCallback(
+    (value: number) => {
+      if (value < minValue) {
+        return minValue
+      }
+
+      if (value > maxValue) {
+        return maxValue
+      }
+
+      return Number.isNaN(value) ? defaultValue : value
+    },
+    [minValue, maxValue, defaultValue],
+  )
+
+  const handleInputBlur = useCallback(() => {
+    if (!inputRef.current) {
+      return
+    }
+
+    const newValue = validateValue(transform(+inputRef.current.value))
+    onValueChange(newValue)
+    setValue(newValue)
+
+    // case: defaultValue === newValue
+    inputRef.current.value = newValue.toString()
+  }, [validateValue, onValueChange])
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = value.toString()
+    }
+  }, [value])
 
   const handleValueChange = useCallback((value: number) => {
     const transformedValue = transform(value)
+
     onValueChange(transformedValue)
     setValue(transformedValue)
   }, [])
 
+  const handleStatusChange = useCallback(() => {
+    if (document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur()
+    }
+  }, [])
+
+  const handleInputKeyDown: KeyboardEventHandler<HTMLInputElement> =
+    useCallback(
+      (ev) => {
+        if (ev.key === 'Enter' || ev.key === 'Escape') {
+          ev.stopPropagation()
+          handleStatusChange()
+        }
+      },
+      [handleStatusChange],
+    )
+
+  const steplines = useMemo(() => {
+    if (!stepValue) {
+      return []
+    }
+
+    const stepAngle =
+      (stepValue / (maxValue - minValue)) * (maxAngle - minAngle)
+
+    return Array.from(
+      {
+        length: Math.ceil((maxValue - minValue) / stepValue) - 1,
+      },
+      (_, i) => (
+        <div
+          className={styles.stepline}
+          style={{
+            transform: `rotate(${minAngle + startAngle + stepAngle * (i + 1)}deg)`,
+          }}
+          key={`stepline-${i}`}
+        />
+      ),
+    )
+  }, [stepValue])
+
   return (
-    <div>
+    <div className={styles.container}>
       <input
+        ref={inputRef}
         className={styles.numberInput}
         type="number"
         defaultValue={defaultValue}
-        value={value}
         min={minValue}
         max={maxValue}
         step={stepValue}
-        onChange={(ev) => handleValueChange(+ev.target.value)}
+        onKeyDown={handleInputKeyDown}
+        onBlur={handleInputBlur}
       />
-      <div className={styles.container}>
+      <div className={styles.knobContainer}>
+        <div
+          className={styles.baseline}
+          style={{ transform: `rotate(${minAngle + startAngle}deg)` }}
+        />
+        <div
+          className={styles.baseline}
+          style={{ transform: `rotate(${maxAngle + startAngle}deg)` }}
+        />
+        {steplines}
         <Knob
           defaultValue={defaultValue}
           value={value}
@@ -57,10 +153,12 @@ export function RotaryKnob({
           startAngle={startAngle}
           stepValue={stepValue}
           onValueChange={handleValueChange}
+          onStatusChange={handleStatusChange}
         >
           <button type="button" className={styles.knob} />
         </Knob>
       </div>
+      <div className={styles.label}>{label}</div>
     </div>
   )
 }
