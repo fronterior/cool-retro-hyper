6b91e41a31540f42ba982b142c6256b278430d08
Low Front <lowfronterior@gmail.com>
2025-02-03 03:36:52 +0900

    feat: add .svg import declare

--- src/types/client.d.ts
+++ src/types/client.d.ts
@@ -1,13 +1,3 @@
-declare module '*.glsl' {
-  const value: string
-  export default value
-}
-
-declare module '*.module.css' {
-  const classes: { [key: string]: string }
-  export default classes
-}
-
 declare module 'xterm' {
   interface Terminal {
     _core: {
@@ -20,4 +10,19 @@ declare module 'electron' {
   interface BrowserWindow {
     rpc: import('electron').IpcMain
   }
+}
+
+declare module '*.glsl' {
+  const value: string
+  export default value
+}
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string }
+  export default classes
+}
+
+declare module '*.svg' {
+  const value: string
+  export default value
 }
